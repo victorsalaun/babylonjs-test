b787b46b2b1345d28fd705ac72cb696c901e85b8
victor.salaun <victor.salaun@gmail.com>
2018-01-26 23:56:01 +0100

    init project with babylonjs playground

---
+++ js/Game.js
@@ -0,0 +1,40 @@
+// Page entièrement chargé, on lance le jeu
+document.addEventListener("DOMContentLoaded", function () {
+    new Game('renderCanvas');
+}, false);
+
+
+Game = function (canvasId) {
+    // Canvas et engine défini ici
+    var canvas = document.getElementById(canvasId);
+    var engine = new BABYLON.Engine(canvas, true);
+    var _this = this;
+
+    // On initie la scène avec une fonction associé à l'objet Game
+    this.scene = this._initScene(engine);
+
+    var _player = new Player(_this, canvas);
+    var _arena = new Arena(_this);
+    // Permet au jeu de tourner
+    engine.runRenderLoop(function () {
+        _this.scene.render();
+    });
+
+    // Ajuste la vue 3D si la fenetre est agrandi ou diminué
+    window.addEventListener("resize", function () {
+        if (engine) {
+            engine.resize();
+        }
+    }, false);
+
+};
+
+
+Game.prototype = {
+    // Prototype d'initialisation de la scène
+    _initScene: function (engine) {
+        var scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color3(0.3, 0.1, 0.6);
+        return scene;
+    }
+};
